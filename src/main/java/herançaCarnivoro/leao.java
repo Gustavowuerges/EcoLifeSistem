b3f76405924dc4fc51849852e720b9aa46b12cf4
NicolasSilvaS <nicolassilvasantos00@gmail.com>
2025-09-08 20:33:17 -0300

    Major bug fixes and new classes such as Carnivoros, EcoInterface, Herbivoros and specific animals. New paths aswell.

---
+++ src/main/java/herançaCarnivoro/leao.java
@@ -0,0 +1,25 @@
+package scr.main.java.herançaCarnivoro;
+import scr.main.java.Carnivoros;
+
+public class leao extends Carnivoros {
+            
+            public void fazerBarulho() {
+                System.out.println("O leão está rugindo.");
+            }
+            public void mover() {
+                System.out.println("O leão está correndo.");
+            }
+
+            public void comer() {
+                System.out.println("O leão está se alimentando.");
+            }
+
+            public void dormir() {
+                System.out.println("O leão está dormindo.");
+            }
+
+            public void cacar() {
+            System.out.println("O leão está caçando.");
+    }
+}  
+
